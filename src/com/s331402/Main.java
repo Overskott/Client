fa8638ba1783c8e2503c6ff792ae0a98c9c4973f
Sebastian Testanière Overskott <overskotts@gmail.com>
2020-02-06 14:58:26 +0100

    prototype client done

--- src/com/s331402/Main.java
+++ src/com/s331402/Main.java
@@ -27,6 +27,22 @@ public class Main {
                 response = input.readLine();
                 System.out.println("Response from server: " + response);
 
+                response = input.readLine();
+
+                System.out.println("Response code: " + response);
+
+                if(response.equals("0")){
+                    while(!(response = input.readLine()).equals("3")) {
+                        System.out.println(response);
+                    }
+                }else if(response.equals("1")){
+                    System.out.println("No email address found on the page!");
+                }else {
+                    System.out.println("Server couldn't find the web page!");
+                }
+
+
+
             } while(!url.equalsIgnoreCase("exit"));
         } catch (IOException e) {
 
