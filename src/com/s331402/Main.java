8b171410016fa0f4df61d23528a92c07ed1e8a03
Vegm <vegard.muller@gmail.com>
2020-02-06 20:46:59 +0100

    fikset to bugs: exit input fungerte ikke riktig, la inn if-sjekk i løkke man kunne legge inn en ugyldig http-adresse som gjorde at serveren hoppet ut av thread

--- src/com/s331402/Main.java
+++ src/com/s331402/Main.java
@@ -10,55 +10,53 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        String hostName = "localhost";
-        if (args.length > 0)
-        {
-            hostName = args[0];
-                if (args.length > 1)
-                {
-                    System.err.println("Usage: java EchoClientTCP [<host name>]");
-                    System.exit(1);
-                }
+        String hostIP = "localhost";
+        int port = 6666;
+
+        if (args.length > 0) {
+            hostIP = args[0];
+            if (args.length > 1) {
+                System.err.println("Please provide only one argument. (Your hosts IP address)");
+                System.exit(1);
+            }
         }
 
-        try (Socket socket = new Socket(hostName, 6666);
-             BufferedReader input = new BufferedReader(
-                     new InputStreamReader(socket.getInputStream()));
+        try (Socket socket = new Socket(hostIP, port);
+             BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
              PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
-             Scanner scanner = new Scanner(System.in)
-        ) {
+             Scanner scanner = new Scanner(System.in)) {
+
             String url;
             String response;
 
             do {
-                System.out.println("Enter URL to extract from: ");
-                url = scanner.nextLine();
+                System.out.println("Enter URL to extract email addresses from: ");
 
+                url = scanner.nextLine();
                 output.println(url);
+
+                if (url.equalsIgnoreCase("exit")) {
+                    break;
+                }
+
                 response = input.readLine();
                 System.out.println("Response from server: " + response);
 
                 response = input.readLine();
-
                 System.out.println("Response code: " + response);
 
-                if(response == null){
-                    System.out.println("Tomt svar fra server");
-                }else if(response.equals("0")){
-                    while(!(response = input.readLine()).equals("3")) {
+                if (response.equals("0")) {
+                    while (!(response = input.readLine()).equals("3")) {
                         System.out.println(response);
                     }
-                }else if(response.equals("1")){
+                } else if (response.equals("1")) {
                     System.out.println("No email address found on the page!");
-                }else if(response.equals("2")){
+                } else if (response.equals("2")) {
                     System.out.println("Server couldn't find the web page!");
                 }
-
-
-
-            } while(!url.equalsIgnoreCase("exit"));
+            } while (!url.equalsIgnoreCase("exit"));
         } catch (IOException e) {
-
+            System.out.println("IOException: " + e.getMessage());
         }
     }
 }
